463ace39c3582de3624fe6ba968183510215e1de
hangdudu <15551359775@163.com>
2023-11-14 00:46:19 +0800

    111

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,42 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    "pages/index/index",
+    "pages/test1/index",
+    "pages/test2/index",
+    "pages/test3/index",
   ],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+  tabBar: {
+    list: [
+      {
+        pagePath: "pages/index/index",
+        text: "首页",
+        iconPath: "assets/images/tabbar/th.jpg",
+        selectedIconPath: "assets/images/tabbar/th.jpg",
+      },
+      {
+        pagePath: "pages/test1/index",
+        text: "测试1",
+        iconPath: "assets/images/tabbar/th.jpg",
+        selectedIconPath: "assets/images/tabbar/th.jpg",
+      },
+      {
+        pagePath: "pages/test2/index",
+        text: "测试2",
+        iconPath: "assets/images/tabbar/th.jpg",
+        selectedIconPath: "assets/images/tabbar/th.jpg",
+      },
+      {
+        pagePath: "pages/test3/index",
+        text: "测试3",
+        iconPath: "assets/images/tabbar/th.jpg",
+        selectedIconPath: "assets/images/tabbar/th.jpg",
+      },
+    ],
+  },
+});
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,16 +1,23 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
+import { View, Text } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import Taro from "@tarojs/taro";
+import "./index.scss";
 
 export default function Index() {
-
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log("Page loaded.");
+  });
+
+  const handleButtonClick = () => {
+    Taro.navigateTo({
+      url: "/pages/test1/index", // 目标页面的路径
+    });
+  };
 
   return (
-    <View className='index'>
+    <View className="index">
       <Text>Hello world!</Text>
+      <button onClick={handleButtonClick}>点我去别的页面</button>
     </View>
-  )
+  );
 }
