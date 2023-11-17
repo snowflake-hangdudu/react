3799a40b98a5d109425e42a1c54c9f3bd2e70970
hangdudu <15551359775@163.com>
2023-11-18 01:46:12 +0800

    11

--- src/app.config.ts
+++ src/app.config.ts
@@ -4,15 +4,27 @@ export default defineAppConfig({
     "pages/test1/index",
     "pages/test2/index",
     "pages/test3/index",
+    "pages/test4/index",
     "pages/form/index",
   ],
+  permission: {
+    "scope.userLocation": {
+      desc: "我就想找事，想看看你位置，你给不给看吧！",
+    },
+  },
   window: {
+    //下拉loading的样式
     backgroundTextStyle: "light",
+    //导航栏背景颜色
     navigationBarBackgroundColor: "#fff",
+    //导航栏标题文字内容
     navigationBarTitleText: "WeChat",
+
+    //导航栏标题颜色
     navigationBarTextStyle: "black",
   },
   tabBar: {
+    custom: false, //自定义tabBar
     list: [
       {
         pagePath: "pages/index/index",
@@ -22,19 +34,26 @@ export default defineAppConfig({
       },
       {
         pagePath: "pages/test1/index",
-        text: "测试1",
+        text: "页面1",
         iconPath: "assets/images/tabbar/th.jpg",
         selectedIconPath: "assets/images/tabbar/th.jpg",
       },
+
       {
         pagePath: "pages/test2/index",
-        text: "测试2",
+        text: "页面2",
         iconPath: "assets/images/tabbar/th.jpg",
         selectedIconPath: "assets/images/tabbar/th.jpg",
       },
       {
         pagePath: "pages/test3/index",
-        text: "测试3",
+        text: "页面3",
+        iconPath: "assets/images/tabbar/th.jpg",
+        selectedIconPath: "assets/images/tabbar/th.jpg",
+      },
+      {
+        pagePath: "pages/test4/index",
+        text: "页面4",
         iconPath: "assets/images/tabbar/th.jpg",
         selectedIconPath: "assets/images/tabbar/th.jpg",
       },
