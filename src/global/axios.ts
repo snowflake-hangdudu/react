9251953e90675d270c4070362a7209d491be4ff1
hangdudu <15551359775@163.com>
2024-04-15 07:30:25 +0800

    11

--- src/global/axios.ts
+++
@@ -1,9 +0,0 @@
-//axios库用于配置请求的接口类型
-import axios, { AxiosRequestConfig } from 'taro-axios';
-import Taro from '@tarojs/taro';
-
-//一个工具类，尚未解释
-import Completer from 'src/core/tools/completer';
-
-
-import Navigate from 'src/core/global/navigate';
